3031d6866fdfed542bacac5b622eac8e842a65c1
Imiklyaeva <Imiklyaeva@aplana.com>
2018-03-15 13:58:36 +0300

    Второе задание. Калькулятор, сложение двух чисел

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+/**
+ * Калькулятор, выполняет сложение двух дробных числе. Задание 2
+ *
+ * @author Irina Miklyaeva
+ * @see #SumMethod(float, float)  Выполняет сложение двух дробных чисел
+ */
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        Scanner scanner = new Scanner(System.in);// Новый объект Scanner
+
+        System.out.println("Введите первое дробное число для сложения (разделитель ,) :");// Ввод из консоли первого дробного числа
+        float i = scanner.nextFloat();
+        System.out.println("Введите второе дробное число для сложения (разделитель ,) :");// Ввод из консоли второго дробного числа
+        float j = scanner.nextFloat();
+        scanner.close();// закрываем сканер
+        float summa=SumMethod(i,j);// вызываем метод для сложения двух дробных чисел и передаем в summa  результат
+        System.out.printf("Сумма=%.4f ", summa);// Вывод Итога сложения двух дробных чисел с точностью до 4 знаков после запятой
     }
+
+    /**
+     * Метод для сложения двух дробных чисел
+     * @param i  первое введенное число
+     * @param j  второе введенное число
+     * @return Результат сложения двух дробных чисел
+     */
+    private static float SumMethod(float i, float j){
+        float sum; // обяъявляем переменную sum
+        sum = i+j ;// выполнякм сложение полученных на вход переменных
+        return sum;}// Возвращаем на выход результат сложения
+
 }
